3cc987b0e4c817969769acf6efe7f37513d6e547
Huanchen Zhai <hczhai@ucla.edu>
2021-09-30 20:43:13 -0700

    fix audit mkl-avx

--- src/pybind.cpp
+++ src/pybind.cpp
@@ -5,6 +5,7 @@
 
 #include "main.hpp"
 #include "matrix_functions.hpp"
+#include <iostream>
 
 namespace py = pybind11;
 using namespace xtest;
@@ -19,6 +20,14 @@ PYBIND11_MODULE(xtest, m)
         .def(py::init<int, int>())
         .def_readwrite("a", &XTest::a)
         .def_readwrite("b", &XTest::b)
+        .def("test", [](XTest *self) {
+            vector<double> x(25);
+            block2::Random::rand_seed(1234);
+            block2::Random::fill_rand_double(x.data(), x.size());
+            cout << x[0] << endl;
+            MatrixFunctions::inverse(MatrixRef(x.data(), 5, 5));
+            cout << x[0] << endl;
+        })
         .def(py::self + py::self);
 
     py::class_<ParallelCommunicator<void>, shared_ptr<ParallelCommunicator<void>>>(
